4585072f444219d476f4ef34f4112cc2da9f3c2c
Arion <87771407+ArionLaw@users.noreply.github.com>
2025-02-08 10:16:25 -0500

    pseudocode for structure

--- Python_Kinematics.py
+++ Python_Kinematics.py
@@ -10,11 +10,15 @@ a = np.array([0,0,0,0,0,0,0]) # z-axis dist diff along x-axis
 alpha = np.array([0,0,PI/2,-PI/2,PI/2,-PI/2,PI/2]) # z-axis angle diff about x-axis
 d = np.array([0,L1,0,L2,0,L3,0]) # x-axis dist diff along z-axis
 theta = np.array([0,0,0,0,0,0,0]) # x-axis angle diff about z-axis
-
+servoOffset = np.array([0,-PI/2,0,-PI/2,0,-PI/2,0]) # offset in 
 
 class FKSolver:
     def FK(joints):
         q = np.array(joints)
+
+        for i in range(1,len(joints),1):
+            #get Ri
+            #get xyzi
         xyz_EE = [x,y,z]
         R1 = RotMtx('x',0)@RotMtx('z',q[1])
         R2 = RotMtx('x',np.pi/2)@RotMtx('z',q[2])
@@ -31,3 +35,25 @@ class IKSolver:
 
 
         return joints
+    
+
+def getTj(k, s, m, h):
+    '''
+    Helper function to get transformation matrix for bending portion.
+    Result depends on if k is 0 (straight) or not.
+    '''
+    if k == 0:
+        res = np.array([[1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, h / m],
+                        [0, 0, 0, 1]
+                        ])
+    else:
+        theta = k*s / m
+        res = np.array([[1, 0, 0, 0],
+                        [0, np.cos(theta), -np.sin(theta),
+                            (np.cos(theta)-1)/k],
+                        [0, np.sin(theta), np.cos(theta), np.sin(theta)/k],
+                        [0, 0, 0, 1]
+                        ])
+    return res
---
+++ Python_Robot_Control.py
@@ -0,0 +1,16 @@
+
+# placeholder states
+receivingSerial = True
+SerialMessageMakesSense = True
+
+while (receivingSerial == True):
+    if (SerialMessageMakesSense == True):
+    # read Arduino Serial
+    # parse?
+    # compute or update current FK
+    # read keyboard input
+    # compute desired IK
+    # write desired joint values to Arduino Serial
+
+    else():
+        # error and exception handling
